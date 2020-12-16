85e14b048d2141c8fb53dded134fd14bd5b67001
elkeis <kiryll.e@gmail.com>
2020-12-16 14:36:15 +0300

    change folder names ignorecase-false

---
+++ fe/src/containers/playlist/index.js
@@ -0,0 +1,43 @@
+import { connect } from 'react-redux';
+import { useEffect } from 'react';
+import Player from '../../components/player';
+import { loadPlaylist } from '../../store/playlist';
+import { Link } from 'react-router-dom';
+import './playlist.scss';
+
+function Playlist ({
+    videos = [],
+    showLoading = false,
+    onLoadPlaylist = () => console.log('loading video data'),
+}) {
+
+    useEffect(() => {
+        onLoadPlaylist();
+    }, [onLoadPlaylist])
+
+    return (
+        <div className="Playlist">
+            {videos.map(video => (
+                <div key={video.id} className="video">
+                    <Player videoURL={video.videoURL} thumbnailURL={video.thumbnailURL}></Player>
+                </div>
+            ))}
+
+            { showLoading ? <div>loading...</div> : null }
+
+            <div className="video">
+                <Link to="/upload">upload new</Link>
+            </div>
+        </div>
+    )
+}
+
+export default connect(
+    state => ({
+        videos: state.playlist.videos,
+        showLoading: state.playlist.showLoading,
+    }),
+    {
+        onLoadPlaylist: loadPlaylist,
+    }
+)(Playlist);
